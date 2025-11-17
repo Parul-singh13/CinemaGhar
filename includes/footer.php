9a409a281f3d7b20fe45aa3d2cdf9e1cf4226786
Parul-singh13 <145196891+Parul-singh13@users.noreply.github.com>
2025-11-17 12:03:12 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+<?php
+  //PHP variable storing property list page path
+  $property_list_path = "/Movieticket/property_list.php";
+?>
+
+<!-- Footer Section -->
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+        <a href="property_list.php?city=Delhi">CinemaGhar in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">CinemaGhar in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">CinemaGhar in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">CinemaGhar in Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2024Copyright CinemaGhar </div>
+    </div>
+</div>
